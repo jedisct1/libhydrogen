a5a10c4d3847206e7156d6e7b2c9f1bfef668ca5
Frank Denis <github@pureftpd.org>
2018-01-14 23:06:22 +0100

    _SUNPRO_CC -> _SUNPRO_C

--- hydrogen.h
+++ hydrogen.h
@@ -12,7 +12,7 @@
 extern "C" {
 #endif
 
-#if !defined(__clang__) && !defined(__GNUC__) && !defined(_SUNPRO_CC)
+#if !defined(__clang__) && !defined(__GNUC__) && !defined(_SUNPRO_C)
 # ifdef __attribute__
 #  undef __attribute__
 # endif
