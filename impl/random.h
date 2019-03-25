8db3df87cd141321fee3f3b1b24bade6df478ba4
Frank Denis <github@pureftpd.org>
2019-03-25 23:59:00 +0100

    wasi support

--- impl/random.h
+++ impl/random.h
@@ -118,6 +118,23 @@ hydro_random_init(void)
         return -1;
     }
     hydro_random_context.counter = ~LOAD64_LE(hydro_random_context.state);
+
+    return 0;
+}
+
+#elif defined(__wasi__)
+
+#include <unistd.h>
+
+static int
+hydro_random_init(void)
+{
+    if (getentropy(hydro_random_context.state,
+                   sizeof hydro_random_context.state) != 0) {
+        return -1;
+    }
+    hydro_random_context.counter = ~LOAD64_LE(hydro_random_context.state);
+
     return 0;
 }
 
