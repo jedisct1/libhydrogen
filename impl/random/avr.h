878e59d1ca24d12ce07e695baa0fc831fbdb925f
DeeFuse <7376678+DeeFuse@users.noreply.github.com>
2021-08-27 14:12:45 +0200

    Replace timer based RNG seed with ADC reference based one, remove "Arduino.h" dependency

--- impl/random/avr.h
+++ impl/random/avr.h
@@ -1,16 +1,49 @@
-#include <Arduino.h>
+// Based on https://electronics.stackexchange.com/a/582756
 
-static bool
-hydro_random_rbit(uint16_t x)
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+static uint8_t
+hydro_random_rbyte()
 {
-    uint8_t x8;
+    uint8_t rnd = 0, low, high;
+    // 20 works really fine for uint8, can be increased to reduce patterns. Each cicle takes
+    // around 80us on 16Mhz main clock which give a random nmumber almost every 1.5ms.
+    int nConversions = 20;
 
-    x8 = ((uint8_t) (x >> 8)) ^ (uint8_t) x;
-    x8 = (x8 >> 4) ^ (x8 & 0xf);
-    x8 = (x8 >> 2) ^ (x8 & 0x3);
-    x8 = (x8 >> 1) ^ x8;
+    for (int i = 0; i < nConversions; i++) {
 
-    return (bool) (x8 & 1);
+        if (i % 2 == 0) {
+            ADMUX = 0b01001110; // High (1.1V)
+        } else {
+            ADMUX = 0b01001111; // Low (0V)
+        }
+
+        ADCSRA |= 1 << ADSC; // Start a conversion
+
+        // ADSC is cleared when the conversion finishes
+        while ((ADCSRA >> ADSC) % 2) {
+            // wait for the conversion to complete
+        }
+
+        low  = ADCL; // do not swap this sequence. Low has to be fetched first.
+        high = ADCH; // the value is always between 0-3
+        rnd ^= low;
+        rnd ^= high;
+
+        // Lets shift rotate the number;
+        uint8_t last = rnd % 2;
+        rnd >>= 1;
+        rnd |= last << 7;
+
+        // Disable the ADC
+        ADCSRA = 0;
+        // Enable the ADC with a randomly selected clock Prescaler between 2 and 128. since each
+        // conversion takes 13 ADC cycles, at 16Mhz system clock, the ADC will now take something in
+        // between 1.6us and 104us for each conversion.
+        ADCSRA = 0b10000000 | ((rnd % 4) << 1);
+    }
+    return rnd;
 }
 
 static int
@@ -19,9 +52,9 @@ hydro_random_init(void)
     const char       ctx[hydro_hash_CONTEXTBYTES] = { 'h', 'y', 'd', 'r', 'o', 'P', 'R', 'G' };
     hydro_hash_state st;
     uint16_t         ebits = 0;
-    uint16_t         tc;
-    bool             a, b;
+    uint8_t          r;
 
+    // Disable Watchdog
     cli();
     MCUSR = 0;
     WDTCSR |= _BV(WDCE) | _BV(WDE);
@@ -31,21 +64,12 @@ hydro_random_init(void)
     hydro_hash_init(&st, ctx, NULL);
 
     while (ebits < 256) {
-        delay(1);
-        tc = TCNT1;
-        hydro_hash_update(&st, (const uint8_t *) &tc, sizeof tc);
-        a = hydro_random_rbit(tc);
-        delay(1);
-        tc = TCNT1;
-        b  = hydro_random_rbit(tc);
-        hydro_hash_update(&st, (const uint8_t *) &tc, sizeof tc);
-        if (a == b) {
-            continue;
-        }
-        hydro_hash_update(&st, (const uint8_t *) &b, sizeof b);
-        ebits++;
+        r = hydro_random_rbyte();
+        hydro_hash_update(&st, (const uint8_t *) &r, sizeof r);
+        ebits += 8;
     }
 
+    // Enable Watchdog
     cli();
     MCUSR = 0;
     WDTCSR |= _BV(WDCE) | _BV(WDE);
@@ -58,4 +82,6 @@ hydro_random_init(void)
     return 0;
 }
 
-ISR(WDT_vect) { }
+ISR(WDT_vect)
+{
+}
