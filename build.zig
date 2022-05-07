58bc4253db571b5cf7958b791c111799b8a2c00b
Frank Denis <github@pureftpd.org>
2022-05-07 18:38:23 +0200

    build.zig: link LibC

--- build.zig
+++ build.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const lib = b.addStaticLibrary("hydrogen", null);
-    lib.addCSourceFile("hydrogen.c", &.{});
+    const lib = b.addStaticLibrary("hydrogen", "hydrogen.c");
+    lib.linkLibC();
     lib.setBuildMode(.ReleaseSmall);
     lib.setTarget(target);
     lib.strip = true;
